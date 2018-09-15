d62c12e1305996db26b09467eed148b331630d4d
Abdulmohsen Alabra <abdulalabra123@gmaile.com>
2018-09-15 16:59:51 -0700

    jkdjk

--- config/orm.js
+++ config/orm.js
@@ -1,17 +1,6 @@
 
 var connection = require("../config/connection.js");
 
-// Create an orm.js file inside config directory.
-
-// Import (require) connection.js into orm.js
-
-// In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
-
-// selectAll()
-// insertOne()
-// updateOne()
-// Export the ORM object in module.exports.
-
 var orm = {
     selectAll: function (cb) {
         var sql = "SELECT * FROM burgers";
@@ -40,12 +29,6 @@ var orm = {
         });
     }
 }
-
-
-   
- 
-
-// INSERT INTO burgers (burger_name) VALUES ('Double Double');
 
 module.exports = orm;
 
