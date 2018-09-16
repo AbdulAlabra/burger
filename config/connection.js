fa0e6b9d7690ae2477ee423cce654a71cf536b5d
Abdulmohsen Alabra <abdulalabra123@gmaile.com>
2018-09-15 19:49:15 -0700

    connection changed

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,42 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+// // Set up MySQL connection.
+// var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Abosaad123",
+//   database: "burgers_db"
+// });
+
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+var mysql = require("mysql");
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+else { 
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Abosaad123",
   database: "burgers_db"
 });
-
-// Make connection.
+};
 connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error("Connection Error" + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log("Connected as id: " + connection.threadId);
 });
 
 module.exports = connection;
